ebeb4f38e57057cbf81fe1fdec6ae59903da8b09
seanmcquaid <sean.mcquaid2@verizon.net>
2018-12-28 15:51:56 -0800

    figured out how to get the coordinates of any address, this should be SUPER useful in practice

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
-const geoCodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBhEyPG1pqRNT2FOdpiMX_2zRDYgY8Y1W0`;
+// this prompts the user to enter the address, maybe set this to the value of an input being entered??
+let address = prompt("What's your address?");
+
+const geoCodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`;
 
 $.getJSON(geoCodeURL, (geoCodeData) => {
     console.log(geoCodeData);
+    // this yields the coordinates for the address input
+    console.log(geoCodeData.results[0].geometry.location);
 });
